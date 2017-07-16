c09ee5d9b0d13a331803d3f2e1f127b43e6334f7
sebrockm <sebrockm@uos.de>
2017-07-16 17:15:47 +0200

    used IteratorAdapter in Yield Iterator

--- Yield.hpp
+++ Yield.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Enumeration.hpp"
+#include "IteratorAdapter.hpp"
 
 #include <memory>
 #include <future>
@@ -26,25 +27,17 @@ namespace Linqpp
 	   Yielder(Yielder const&) = delete;
 	   Yielder& operator=(Yielder const&) = delete;
 
-	   class Iterator : public std::iterator<std::input_iterator_tag, T>
+	   class Iterator : public std::iterator<std::input_iterator_tag, T>, public IteratorAdapter<Iterator>
 	   {
 	   private:
 		  std::shared_ptr<Yielder> _spYielder;
 		  bool _isEnd;
 
-		  void Advance()
-		  {
-			 {
-				std::unique_lock<std::mutex> lock(_spYielder->_mutex);
-				if (!_spYielder->_buffer.empty()) // should be empty only in the very first call of Advance
-				   _spYielder->_buffer.pop();
-				_spYielder->_cv.wait(lock, [=]() { return _spYielder->_finishedGenerating || !_spYielder->_buffer.empty(); });
-				
-				if (_spYielder->_buffer.empty())
-				   _isEnd = true;
-			 }
-			 _spYielder->_cv.notify_all();
-		  }
+	   public:
+		  Iterator() : _isEnd(true) { };
+		  Iterator(std::shared_ptr<Yielder> const& spYielder) : _spYielder(spYielder), _isEnd(false) { Increment(); }
+
+          bool Equals(Iterator const& other) const { return _isEnd == other._isEnd; }
 
 		  decltype(auto) Get()
 		  {
@@ -58,18 +51,19 @@ namespace Linqpp
 			 return _spYielder->_buffer.front();
 		  }
 
-	   public:
-		  Iterator() : _isEnd(true) { };
-		  Iterator(std::shared_ptr<Yielder> const& spYielder) : _spYielder(spYielder), _isEnd(false) { Advance(); }
-
-		  decltype(auto) operator*() { return Get(); }
-		  decltype(auto) operator*() const { return Get(); }
-		  decltype(auto) operator->() { return std::addressof(Get()); }
-		  decltype(auto) operator->() const { return std::addressof(Get()); }
-		  Iterator& operator++() { Advance(); return *this; }
-		  Iterator operator++(int) { auto copy = *this; Advance(); return copy; }
-		  bool operator==(Iterator const& other) const { return _isEnd == other._isEnd; }
-		  bool operator!=(Iterator const& other) const { return !(*this == other); }
+		  void Increment()
+		  {
+			 {
+				std::unique_lock<std::mutex> lock(_spYielder->_mutex);
+				if (!_spYielder->_buffer.empty()) // should be empty only in the very first call of Advance
+				   _spYielder->_buffer.pop();
+				_spYielder->_cv.wait(lock, [=]() { return _spYielder->_finishedGenerating || !_spYielder->_buffer.empty(); });
+				
+				if (_spYielder->_buffer.empty())
+				   _isEnd = true;
+			 }
+			 _spYielder->_cv.notify_all();
+		  }
 	   };
 
 	   friend class Iterator;
